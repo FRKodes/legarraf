2501aa4fe1d84283f4bb0ec8e2fcb376872f1ba2
frkodes <frkalderon@gmail.com>
2016-08-09 12:16:09 -0500

    Adding google verification meta tag

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="google-site-verification" content="vmNJB_WiIF1vqW_I2QpsbaDyVnxGcVba-jTRnDM3YzU" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Le Garraf son las mejores nieves de garrafa en Guadalajara, variedad de sabores naturales con especialidades únicas como cazuelitas de chamo con nieve de pulparindo, y para los que se cuidan nieve endulzada con splenda. Servimos te evento con el mejor servicio.">
 <meta name="keywords" content="nieves ,nieves de garrafa,nieves de garrafa en guadalajara,le garraf,nieves de garrafa en providencia,nieves de garrafa para eventos,nieves artesanales,nieves en andares ,helados en andares,helados para eventos,helados en guadalajara ,helados de garrafa ,nieves de garrafa en andares,nieves con splenda,nieves sin calorias,nives con chamoy,helados con chamoy,cazuelitas de chamoy,nieves de garrafa con chamoy,nives de garrafa pulparindo,aguas frescas en guadalajara,topping mix,garrafon,cookie sandwich,nieves naturales,nieves con toppings,ice cream,paletas,paletas de hielo">
